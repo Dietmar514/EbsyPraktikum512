f397994d48078282bab96baeac5fba608e0347a6
Djoser6 <stephan.wachter95@gmx.de>
2020-10-31 16:58:32 +0100

    entwurf Praktikum 2

--- main.c
+++ main.c
@@ -2,12 +2,23 @@
 #include <LPC177x_8x.h>
 #include "delay.h"
 
+#define processCount 8;
+#define delay 300;	//delay in ms
+#define freq 5;
+
+#define led1_value 0x0100;
+#define led2_value 0x0200;
+#define led3_value 0x0400;
+#define led4_value 0x0800;
+#define led5_value 0x1000;
+#define led6_value 0x2000;
+#define led7_value 0x4000;
+#define led8_value 0x8000;
 
 uint32_t tickcounter = 0;
 
-#define led1_freq 1;
-#define led2_freq 6;
-#define delay 300;	//delay in ms
+void (*processes[processCount])(int) = { controllLed1, controllLed2, controllLed3, controllLed4, controllLed5, controllLed6, controllLed7, controllLed8 };
+
 
 void init(void) 
 {
@@ -15,83 +26,120 @@ void init(void)
 	LPC_GPIO0->CLR = 0xFFFFFFFF;
 }
 
-void led1(void) 
-{
-	static uint32_t led_value;
-	static uint32_t led_trigger;	//toggles led direction
-	if(tickcounter == 0)			//Initialize values
+
+
+void controllLed1(int led_freq)			//duty-cycle = timeActive/cycleLength
+{		
+	if (tickcounter < led_freq == 0)
 	{
-		led_value = 0x100;
-		led_trigger = 0;
+		LPC_GPIO0->SET = led1_value;
+	}
+	else
+	{
+		LPC_GPIO0->CLR = led1_value;
 	}
 
-	LPC_GPIO0->CLR = led_value;
-	if (led_trigger == 0) 
-	{
-		led_value = led_value << 1;
-		if (led_value == 0x800) 
-		{
-			led_trigger = 1;
-		}
-	}
-	else if (led_trigger == 1) 
-	{
-		led_value = led_value >> 1;
-		if (led_value == 0x100) 
-		{
-			led_trigger = 0;
-		}
-	}
-	LPC_GPIO0->SET = led_value;
 }
 
-
-
-void led2(void) 
+void controllLed2(int led_freq)
 {
-	static uint32_t led_value;
-	static uint32_t led_trigger;	//toggles led direction
-	if (tickcounter == 0) 			//Initialize values
+	if (tickcounter < led_freq == 0)
 	{
-		led_value = 0x1000;
-		led_trigger = 0;
+		LPC_GPIO0->SET = led2_value;
 	}
-
-	LPC_GPIO0->CLR = led_value;
-	if (led_trigger == 0)
+	else
 	{
-		led_value = led_value << 1;
-		if (led_value == 0x8000)
-		{
-			led_trigger = 1;
-		}
+		LPC_GPIO0->CLR = led2_value;
 	}
-	else if (led_trigger == 1) 
-	{
-		led_value = led_value >> 1;
-		if (led_value == 0x1000) 
-		{
-			led_trigger = 0;
-		}
-	}
-	LPC_GPIO0->SET = led_value;
 }
+
+void controllLed3(int led_freq)
+{
+	if (tickcounter < led_freq == 0)
+	{
+		LPC_GPIO0->SET = led3_value;
+	}
+	else
+	{
+		LPC_GPIO0->CLR = led3_value;
+	}
+}
+
+void controllLed4(int led_freq)
+{
+	if (tickcounter < led_freq == 0)
+	{
+		LPC_GPIO0->SET = led4_value;
+	}
+	else
+	{
+		LPC_GPIO0->CLR = led4_value;
+	}
+}
+
+void controllLed5(int led_freq)
+{
+	if (tickcounter < led_freq == 0)
+	{
+		LPC_GPIO0->SET = led5_value;
+	}
+	else
+	{
+		LPC_GPIO0->CLR = led5_value;
+	}
+}
+
+void controllLed6(int led_freq)
+{
+	if (tickcounter < led_freq == 0)
+	{
+		LPC_GPIO0->SET = led6_value;
+	}
+	else
+	{
+		LPC_GPIO0->CLR = led6_value;
+	}
+}
+
+void controllLed7(int led_freq)
+{
+	if (tickcounter < led_freq == 0)
+	{
+		LPC_GPIO0->SET = led7_value;
+	}
+	else
+	{
+		LPC_GPIO0->CLR = led7_value;
+	}
+}
+
+void controllLed8(int led_freq)
+{
+	if (tickcounter < led_freq == 0)
+	{
+		LPC_GPIO0->SET = led8_value;
+	}
+	else
+	{
+		LPC_GPIO0->CLR = led8_value;
+	}
+}
+
 
 int main(void)
 {
 	init();
 	while (1)
 	{
-		if(tickcounter % led1_freq == 0)
+		for (size_t i = 0; i < length; i++)
 		{
-			led1();
-		}
-		if(tickcounter % led2_freq == 0)
-		{
-			led2();
+			for (int currTask = 0; currTask < processCount; currTask++)
+			{ 
+				(*tasklist[currTask])(freq);
+			}
 		}
 		tickcounter++;
-		delayms(delay);
+		tickcount %= 100;
 	}
 }
 
